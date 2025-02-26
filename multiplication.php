688b93e9fc9d74c81beb960aec554e55dc1090e1
Didier Carrillo <didiercarrillo40@gmail.com>
2025-02-26 17:51:27 -0600

    Moved hello world php code into multiplication.php

--- multiplication.php
+++ multiplication.php
@@ -1,3 +1,9 @@
-<?php
-    echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Multiplication Table</title>
+    </head>
+    <body>
+        <?php echo '<p>multiplication</p>'; ?>
+    </body>
+</html>
