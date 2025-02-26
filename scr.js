e3d75d3e89c4b48117b6f41e539c6fe33998ab23
Didier Carrillo <didiercarrillo40@gmail.com>
2025-02-25 18:40:43 -0600

    used getElementById() method for both passwords

--- scr.js
+++ scr.js
@@ -1,3 +1,9 @@
 function compare(string1, string2){
     
 }
+
+function get_Input(){
+    var input_box1 = document.getElementById("password1");
+    var input_box2 = document.getElementById("password2");
+    console.log(input_box1.value);
+}
