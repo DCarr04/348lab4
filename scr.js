df461840a57988b97c0edad273c42e92cbfd8630
Didier Carrillo <didiercarrillo40@gmail.com>
2025-02-25 18:49:18 -0600

    printed second password to console

--- scr.js
+++ scr.js
@@ -6,4 +6,5 @@ function get_Input(){
     var input_box1 = document.getElementById("password1");
     var input_box2 = document.getElementById("password2");
     console.log(input_box1.value);
+    console.log(input_box2.value);
 }
